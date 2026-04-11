9a55efd39e55f6e74da498c03d44cb1922ac63fe
Jacinth <jacinthcedricbarral@gmail.com>
2026-04-11 16:03:01 +0800

    feat[Jacinth]: add admin authorization to sit-in creation and end session

--- api/sitin/end_session.php
+++ api/sitin/end_session.php
@@ -3,6 +3,8 @@
 require_once '../../includes/cors.php';
 require_once '../../includes/initialize.php';
 
+requireAdmin();
+
 $data = json_decode(file_get_contents("php://input"));
 
 if (empty($data->log_id)) {
